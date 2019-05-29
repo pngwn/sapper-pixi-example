c9e6f3d66926f03ab24214f0847a4ae86263336a
pngwn <hello@pngwn.io>
2019-05-29 18:43:05 +0100

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,86 @@
+<script>
+  import { onMount } from "svelte";
+
+  let PIXI, div;
+
+  onMount(async () => {
+    PIXI = await import("pixi.js");
+    //console.log(PIXI);
+
+    // The application will create a renderer using WebGL, if possible,
+    // with a fallback to a canvas render. It will also setup the ticker
+    // and the root stage PIXI.Container
+    const app = new PIXI.Application();
+
+    // The application will create a canvas element for you that you
+    // can then insert into the DOM
+    div.appendChild(app.view);
+
+    // load the texture we need
+    app.loader.add("bunny", "bunny.png").load((loader, resources) => {
+      // This creates a texture from a 'bunny.png' image
+      const bunny = new PIXI.Sprite(resources.bunny.texture);
+
+      // Setup the position of the bunny
+      bunny.x = app.renderer.width / 2;
+      bunny.y = app.renderer.height / 2;
+
+      // Rotate around the center
+      bunny.anchor.x = 0.5;
+      bunny.anchor.y = 0.5;
+
+      // Add the bunny to the scene we are building
+      app.stage.addChild(bunny);
+
+      // Listen for frame updates
+      app.ticker.add(() => {
+        // each frame we spin the bunny around a bit
+        bunny.rotation += 0.01;
+      });
+    });
+  });
+</script>
+
+<style>
+  h1,
+  figure,
+  p {
+    text-align: center;
+    margin: 0 auto;
+  }
+
+  h1 {
+    font-size: 2.8em;
+    text-transform: uppercase;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+  }
+
+  figure {
+    margin: 0 0 1em 0;
+  }
+
+  img {
+    width: 100%;
+    max-width: 400px;
+    margin: 0 0 1em 0;
+  }
+
+  p {
+    margin: 1em auto;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 4em;
+    }
+  }
+</style>
+
+<svelte:head>
+  <title>Sapper project template</title>
+</svelte:head>
+
+<h1>Great success!</h1>
+
+<div bind:this={div} />
